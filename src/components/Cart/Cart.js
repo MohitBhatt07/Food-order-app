0f5c6fb95dbe5084e7c57b115ead07b0785917f5
Mohit Bhatt <bhattmohit869@gmail.com>
2023-08-10 11:24:00 +0530

    Customer details to backend
    Submitted customer details and its orders to backend firebase.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -25,6 +25,16 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  const submitOrderHandler = (userData) => {
+    fetch('https://food-delivery-cafa8-default-rtdb.firebaseio.com/consumerDetails.json', {
+      method: 'POST',
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items
+      })
+    });
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
       {cartCtx.items.map((item) => (
@@ -55,13 +65,18 @@ const Cart = (props) => {
 
   return (
     <Modal onClose={props.onClose}>
+      
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
+      
+      
     </Modal>
   );
 };
